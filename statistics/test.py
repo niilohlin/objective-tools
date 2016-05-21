9bb76df67c436d091d85d75c6968ede89d9194b7
niil <niilohlin@gmail.com>
2016-05-21 11:09:31 +0200

    add testing framework

--- statistics/test.py
+++ statistics/test.py
@@ -1,13 +1,34 @@
+import unittest
 from numberOfPublicMethods import *
 
-def headersIsClass():
-    return headers()[0] == "class.h"
+class Tests(unittest.TestCase):
 
-def propertiesIs4():
-    return parseFile("class.h") == ObjectiveCClass(publicMethods=4, className="TestClass")
+    @staticmethod
+    def listOfOCClasses():
+        return [
+                ObjectiveCClass(publicMethods=4, className="TestClass4"),
+                ObjectiveCClass(publicMethods=5, className="TestClass5"),
+                ObjectiveCClass(publicMethods=2, className="TestClass2"),
+                ObjectiveCClass(publicMethods=1, className="TestClass1"),
+                ObjectiveCClass(publicMethods=4, className="TestClass3")]
+
+    def test_headersIsClass(self):
+        self.assertEqual(headers()[0], "class.h")
+
+    def test_propertiesIs4(self):
+        self.assertEqual(parseFile("class.h"), ObjectiveCClass(publicMethods=4, className="TestClass"))
+
+
+    def test_average(self):
+        self.assertEqual(calculateAveragePublic([ObjectiveCClass(publicMethods=5, className="")]), 5)
+
+    def test_findWorst(self):
+        classes = Tests.listOfOCClasses()
+        self.assertEqual(findMostPublic(classes, 1), [ObjectiveCClass(publicMethods=5, className="TestClass5")])
+
+
+
 
 if __name__ == "__main__":
-    print(headersIsClass())
-    print(propertiesIs4())
-    print(calculateAveragePublic([ObjectiveCClass(publicMethods=5, className="")]) == 5)
+    unittest.main()
 
